d6d024b0cff553bb3c25b2aad877e9c097b95559
Ruth-F <r0583208975@gmail.com>
2021-01-10 11:04:37 +0200

    bbb

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
-        <h1>aaa</h1>
+        <h1>bbb</h1>
         <img className={styles.image} src={coronaImg} alt="COVID-19"></img>
         <Cards data={data} />
         <CountryPicer handleCountryChange={this.handleCountryChange} />
